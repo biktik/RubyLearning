61131cfff7178125ef70d4e3523bc0e7c2e65e25
biktik <biktik@gmail.com>
2012-07-29 06:19:20 -0700

    Started my first ruby on rails application.
    Simple blogging type ror app.

---
+++ app1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App1::Application.config.secret_token = '87d79f2372b316bf245df0e3da60f043ebd3899ea31474ca8f62fa7583d6f57b689d799639111960133349e8e2a9a7534fb1ab8c53cde2ba4f0bf8c6fdff4ac3'
